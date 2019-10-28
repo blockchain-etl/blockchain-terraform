61be4c1ee900d6b61f251d924bd0c5ab9c8734ee
Aleksandr Vorona <av@arilot.com>
2019-10-28 21:02:42 +0200

    add compute service into address resources

--- infra/ip.tf
+++ infra/ip.tf
@@ -1,20 +1,24 @@
 resource "google_compute_address" "btc-0" {
-  project = var.GCP_PROJECT_ID
-  name    = "${var.IP_ADDRESS_NAME_PREFIX}-btc-0"
-  region  = var.IP_ADDRESS_REGION
+  project    = var.GCP_PROJECT_ID
+  depends_on = ["google_project_service.compute_svc"]
+  name       = "${var.IP_ADDRESS_NAME_PREFIX}-btc-0"
+  region     = var.IP_ADDRESS_REGION
 }
 resource "google_compute_address" "btc-1" {
-  project = var.GCP_PROJECT_ID
-  name    = "${var.IP_ADDRESS_NAME_PREFIX}-btc-1"
-  region  = var.IP_ADDRESS_REGION
+  project    = var.GCP_PROJECT_ID
+  depends_on = ["google_project_service.compute_svc"]
+  name       = "${var.IP_ADDRESS_NAME_PREFIX}-btc-1"
+  region     = var.IP_ADDRESS_REGION
 }
 resource "google_compute_address" "eth-0" {
-  project = var.GCP_PROJECT_ID
-  name    = "${var.IP_ADDRESS_NAME_PREFIX}-eth-0"
-  region  = var.IP_ADDRESS_REGION
+  project    = var.GCP_PROJECT_ID
+  depends_on = ["google_project_service.compute_svc"]
+  name       = "${var.IP_ADDRESS_NAME_PREFIX}-eth-0"
+  region     = var.IP_ADDRESS_REGION
 }
 resource "google_compute_address" "eth-1" {
-  project = var.GCP_PROJECT_ID
-  name    = "${var.IP_ADDRESS_NAME_PREFIX}-eth-1"
-  region  = var.IP_ADDRESS_REGION
+  project    = var.GCP_PROJECT_ID
+  depends_on = ["google_project_service.compute_svc"]
+  name       = "${var.IP_ADDRESS_NAME_PREFIX}-eth-1"
+  region     = var.IP_ADDRESS_REGION
 }
