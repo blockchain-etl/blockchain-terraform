349920ff5b96fb45bc298a70c5393ed1ceee06ca
Aleksandr Vorona <av@arilot.com>
2019-10-29 03:57:55 +0200

    do not disable services on destroy

--- infra/service.tf
+++ infra/service.tf
@@ -1,20 +1,20 @@
 resource "google_project_service" "iam_svc" {
-  project = "${var.GCP_PROJECT_ID}"
-  service = "iam.googleapis.com"
-
+  project                    = "${var.GCP_PROJECT_ID}"
+  service                    = "iam.googleapis.com"
+  disable_on_destroy         = false
   disable_dependent_services = true
 }
 
 resource "google_project_service" "compute_svc" {
-  project = var.GCP_PROJECT_ID
-  service = "compute.googleapis.com"
-
+  project                    = var.GCP_PROJECT_ID
+  service                    = "compute.googleapis.com"
+  disable_on_destroy         = false
   disable_dependent_services = true
 }
 
 resource "google_project_service" "container_svc" {
-  project = var.GCP_PROJECT_ID
-  service = "container.googleapis.com"
-
+  project                    = var.GCP_PROJECT_ID
+  service                    = "container.googleapis.com"
+  disable_on_destroy         = false
   disable_dependent_services = true
 }
