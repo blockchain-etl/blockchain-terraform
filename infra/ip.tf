4639071f1dc938391bc2a2beac3a274028cea35d
Aleksandr Vorona <av@arilot.com>
2019-11-05 07:32:05 +0200

    add litecoin IPs

--- infra/ip.tf
+++ infra/ip.tf
@@ -34,3 +34,15 @@ resource "google_compute_address" "bch-1" {
   name       = "${var.IP_ADDRESS_NAME_PREFIX}-bch-1"
   region     = var.IP_ADDRESS_REGION
 }
+resource "google_compute_address" "ltc-0" {
+  project    = var.GCP_PROJECT_ID
+  depends_on = ["google_project_service.compute_svc"]
+  name       = "${var.IP_ADDRESS_NAME_PREFIX}-ltc-0"
+  region     = var.IP_ADDRESS_REGION
+}
+resource "google_compute_address" "ltc-1" {
+  project    = var.GCP_PROJECT_ID
+  depends_on = ["google_project_service.compute_svc"]
+  name       = "${var.IP_ADDRESS_NAME_PREFIX}-ltc-1"
+  region     = var.IP_ADDRESS_REGION
+}
