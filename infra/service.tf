4a380958756db3de9731378c9a7cdb41d79b7506
Aleksandr Vorona <av@arilot.com>
2019-11-09 00:49:37 +0200

    pubsub initial

--- infra/service.tf
+++ infra/service.tf
@@ -18,3 +18,10 @@ resource "google_project_service" "container_svc" {
   disable_on_destroy         = false
   disable_dependent_services = true
 }
+
+resource "google_project_service" "pubsub_svc" {
+  project                    = var.GCP_PROJECT_ID
+  service                    = "pubsub.googleapis.com"
+  disable_on_destroy         = false
+  disable_dependent_services = true
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -66,3 +66,36 @@ variable "IP_ADDRESS_NAME_PREFIX" {
   default = ""
   type    = string
 }
+
+variable "BTCL_CHAINS" {
+  default = ["bitcoin", "bitcoin_cash", "dogecoin", "litecoin", "dash", "zcash"]
+  type    = list(string)
+}
+
+//variable "BTCL_ENTITY_TYPES" {
+//  default = ["blocks", "transactions"]
+//  type    = list(string)
+//}
+//
+//variable "ETHL_CHAINS" {
+//  default = ["ethereum"]
+//  type    = list(string)
+//}
+
+variable "ETHL_ENTITY_TYPES" {
+  default = ["blocks", "transactions", "logs", "token_transfers", "traces", "contracts", "tokens"]
+  type    = list(string)
+}
+
+variable "PUBSUB" {
+  # 7 days
+  default = {
+    message_retention_duration = "604800s"
+    retain_acked_messages      = true
+    ack_deadline_seconds       = 30
+    topic_name_prefix          = "crypto_"
+    subscription_name_prefix   = "crypto_"
+    subscription_name_suffix   = "dataflow.bigquery"
+  }
+  type = map(any)
+}
