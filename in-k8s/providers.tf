026a926c0246046f70a5be7ea0f73f421301c227
Alex Vorona <av@dysnix.com>
2020-02-25 13:18:33 +0200

    pin provider versions, typo fix

--- in-k8s/providers.tf
+++ in-k8s/providers.tf
@@ -1,8 +1,14 @@
 provider "kubernetes" {
   config_context = var.K8S_CONTEXT
+  version        = "1.10"
 }
 
 provider "helm" {
   service_account = "tiller"
   install_tiller  = true
+  version         = "1.0"
+}
+
+provider "null" {
+  version = "~> 2.1"
 }
--- infra/provider.tf
+++ infra/provider.tf
@@ -2,6 +2,22 @@
 provider "google" {
   project = var.GCP_PROJECT_ID
   # gke requires this, incorrectly?
-  region = var.GKE_MASTER_REGION
+  region  = var.GKE_MASTER_REGION
+  version = "3.9"
 }
 
+provider "local" {
+  version = "~> 1.4"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
